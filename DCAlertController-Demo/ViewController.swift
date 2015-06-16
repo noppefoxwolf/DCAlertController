de0739a972ceac055559da44a10c80c67f8f8287
hirano_tomoya <cromteria@gmail.com>
2015-06-17 07:12:51 +0900

    Initial Commit

---
+++ DCAlertController-Demo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  DCAlertController-Demo
+//
+//  Created by Tomoya_Hirano on H27/06/17.
+//  Copyright (c) 平成27年 Tomoya_Hirano. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
